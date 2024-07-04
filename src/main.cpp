b491d07b7af35330470ca63481bde43c3d624207
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2024-07-04 13:08:58 -0300

    Implementa main de teste

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,16 @@
 // Created by pires on 6/28/2024.
 //
 
+#include "BoardGame.hpp"
+#include "Player.hpp"
+
 int main(int argc, char* argv[]) {
+    Player player1 = Player("J1", "Pedro");
+    Player player2 = Player("J2", "João");
+
+    BoardGame boardGame = BoardGame(player1, player2);
+
+    boardGame.playGame();
+
     return 0;
 }
