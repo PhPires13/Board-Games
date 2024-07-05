7146abff50f04fac392906cde538e36b37c47e65
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2024-07-04 23:28:17 -0300

    Update comment ident

--- include/BoardGame.hpp
+++ include/BoardGame.hpp
@@ -42,67 +42,67 @@ protected:
 
 public:
     /*
-    * Create a BoardGame instance
-    *
-    * @param _player1: Player 1 of the game
-    * @param _player2: Player 2 of the game
-    * @param _boardWidth: Width of the board
-    * @param _boardHeight: Height of the board
-    * @param symbol1: Symbol of player 1
-    * @param symbol2: Symbol of player 2
-    */
+     * Create a BoardGame instance
+     *
+     * @param _player1: Player 1 of the game
+     * @param _player2: Player 2 of the game
+     * @param _boardWidth: Width of the board
+     * @param _boardHeight: Height of the board
+     * @param symbol1: Symbol of player 1
+     * @param symbol2: Symbol of player 2
+     */
     BoardGame(Player& _player1, Player& _player2, int _boardWidth = BoardGame::defaultBoardWidth,
     int _boardHeight = BoardGame::defaultBoardHeight, char symbol1 = BoardGame::defaultSymbol1,
     char symbol2 = BoardGame::defaultSymbol2);
 
     /*
-    * Print the current board state
-    */
+     * Print the current board state
+     */
     void printBoard() const;
 
     /*
-    * Read a move from the whole input line (int values separated by space)
-    *
-    * @return: A vector with the move coordinates
-    */
+     * Read a move from the whole input line (int values separated by space)
+     *
+     * @return: A vector with the move coordinates
+     */
     static std::vector<int> readMove();
 
     /*
-    * Check if the move is valid
-    *
-    * @param move: The move to be checked
-    *
-    * @return: MovieStatus enum
-    */
+     * Check if the move is valid
+     *
+     * @param move: The move to be checked
+     *
+     * @return: MovieStatus enum
+     */
     virtual MoveStatus isMoveValid(const std::vector<int>& move) const;
 
     /*
-    * Execute move on the board
-    *
-    * @param move: The move to be executed
-    * @param symbol: The symbol to be placed on the board
-    */
+     * Execute move on the board
+     *
+     * @param move: The move to be executed
+     * @param symbol: The symbol to be placed on the board
+     */
     virtual void makeMove(const std::vector<int>& move, char symbol);
 
     /*
-    * Check if the game is over
-    *
-    * @return: GameState enum
-    */
+     * Check if the game is over
+     *
+     * @return: GameState enum
+     */
     virtual GameState getGameState() const;
 
     /*
-    * Check whose turn is it
-    *
-    * @Param turn: The current turn number
-    */
+     * Check whose turn is it
+     *
+     * @param turn: The current turn number
+     */
     Player& whoseTurn(int turn) const;
 
     /*
-    * Play the game
-    *
-    * @return: The game state at the end
-    */
+     * Play the game
+     *
+     * @return: The game state at the end
+     */
     GameState playGame();
 };
 
