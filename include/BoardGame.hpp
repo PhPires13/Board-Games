5ea92dfe5396c94a117f84bbda402274be87765b
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2024-07-08 15:19:14 -0300

    Fix comment notation | add unordered_map include

--- include/BoardGame.hpp
+++ include/BoardGame.hpp
@@ -22,7 +22,7 @@ enum GameState {
     TIE = 3,
 };
 
-/*
+/**
  * Class to represent a board game, it is responsible for controlling all the processes of the game and the players in the match
  */
 class BoardGame {
@@ -41,67 +41,67 @@ protected:
     std::vector<std::vector<char>> board;
 
 public:
-    /*
+    /**
      * Create a BoardGame instance
      *
-     * @param _player1: Player 1 of the game
-     * @param _player2: Player 2 of the game
-     * @param _boardWidth: Width of the board
-     * @param _boardHeight: Height of the board
-     * @param symbol1: Symbol of player 1
-     * @param symbol2: Symbol of player 2
+     * @param _player1 Player 1 of the game
+     * @param _player2 Player 2 of the game
+     * @param _boardWidth Width of the board
+     * @param _boardHeight Height of the board
+     * @param symbol1 Symbol of player 1
+     * @param symbol2 Symbol of player 2
      */
     BoardGame(Player& _player1, Player& _player2, int _boardWidth = BoardGame::defaultBoardWidth,
     int _boardHeight = BoardGame::defaultBoardHeight, char symbol1 = BoardGame::defaultSymbol1,
     char symbol2 = BoardGame::defaultSymbol2);
 
-    /*
+    /**
      * Print the current board state
      */
     void printBoard() const;
 
-    /*
+    /**
      * Read a move from the whole input line (int values separated by space)
      *
-     * @return: A vector with the move coordinates
+     * @return A vector with the move coordinates
      */
     static std::vector<int> readMove();
 
-    /*
+    /**
      * Check if the move is valid
      *
-     * @param move: The move to be checked
+     * @param move The move to be checked
      *
-     * @return: MovieStatus enum
+     * @return MovieStatus enum
      */
     virtual MoveStatus isMoveValid(const std::vector<int>& move) const;
 
-    /*
+    /**
      * Execute move on the board
      *
-     * @param move: The move to be executed
-     * @param symbol: The symbol to be placed on the board
+     * @param move The move to be executed
+     * @param symbol The symbol to be placed on the board
      */
     virtual void makeMove(const std::vector<int>& move, char symbol);
 
-    /*
+    /**
      * Check if the game is over
      *
-     * @return: GameState enum
+     * @return GameState enum
      */
     virtual GameState getGameState() const;
 
-    /*
+    /**
      * Check whose turn is it
      *
-     * @param turn: The current turn number
+     * @param turn The current turn number
      */
     Player& whoseTurn(int turn) const;
 
-    /*
+    /**
      * Play the game
      *
-     * @return: The game state at the end
+     * @return The game state at the end
      */
     GameState playGame();
 };
--- include/Player.hpp
+++ include/Player.hpp
@@ -7,6 +7,7 @@
 
 #include <list>
 #include <string>
+#include <unordered_map>
 
 enum CreationStatus {
     CREATED,
@@ -20,7 +21,7 @@ enum DeletionStatus {
     DELETION_ERROR
 };
 
-/*
+/**
  * Class that represents the player, it is also responsible for allowing the interaction with the database
  */
 class Player {
@@ -33,134 +34,134 @@ class Player {
     char symbol;
 
 public:
-    /*
+    /**
      * Create a new player instance
      *
-     * @param _nick: Player's nickname
-     * @param _name: Player's name
-     * @param _wins: Player's wins stats
-     * @param _losses: Player's losses stats
+     * @param _nick Player's nickname
+     * @param _name Player's name
+     * @param _wins Player's wins stats
+     * @param _losses Player's losses stats
      */
     Player(const std::string& _nick, const std::string& _name, const std::unordered_map<char, uint>& _wins = {}, const std::unordered_map<char, uint>& _losses = {});
 
-    /*
+    /**
      * Get the player's nickname
      *
-     * @return: The player's nickname
+     * @return The player's nickname
      */
     std::string getNick() const;
 
-    /*
+    /**
      * Get the player's name
      *
-     * @return: The player's name
+     * @return The player's name
      */
     std::string getName() const;
 
-    /*
+    /**
      * Get the player's wins
      *
-     * @return: The player's wins stats
+     * @return The player's wins stats
      */
     std::unordered_map<char, uint> getWins() const;
 
-    /*
+    /**
      * Get the player's losses
      *
-     * @return: The player's losses stats
+     * @return The player's losses stats
      */
     std::unordered_map<char, uint> getLosses() const;
 
-    /*
+    /**
      * Get the player's wins
      *
-     * @param game: The game to get the wins
+     * @param game The game to get the wins
      *
-     * @return: The player's wins
+     * @return The player's wins
      */
     uint getWins(char game) const;
 
-    /*
+    /**
      * Get the player's losses
      *
-     * @param game: The game to get the losses
+     * @param game The game to get the losses
      *
-     * @return: The player's losses stats
+     * @return The player's losses stats
      */
     uint getLosses(char game) const;
 
-    /*
+    /**
      * Add a win to the player
      *
-     * @param game: The game to add the win
+     * @param game The game to add the win
      */
     void addWin(char game);
 
-    /*
+    /**
      * Add a loss to the player
      *
-     * @param game: The game to add the loss
+     * @param game The game to add the loss
      */
     void addLoss(char game);
 
-    /*
+    /**
      * Set the player's symbol
      *
-     * @param symbol: The symbol to be set
+     * @param symbol The symbol to be set
      */
     void setSymbol(char symbol);
 
-    /*
+    /**
      * Get the player's symbol
      *
-     * @return: The player's symbol
+     * @return The player's symbol
      */
     char getSymbol() const;
 
 
-    /*
+    /**
      * Create a new player in the database
      *
-     * @param _nick: The player's nickname
-     * @param _name: The player's name
+     * @param nick The player's nickname
+     * @param name The player's name
      *
-     * @return: CreationStatus enum
+     * @return CreationStatus enum
      */
     static CreationStatus createPlayer(const std::string& nick, const std::string& name);
 
-    /*
+    /**
      * Load a player from the database
      *
-     * @param nick: The player's nickname
+     * @param nick The player's nickname
      *
-     * @return: The player loaded
+     * @return The player loaded
      */
     static Player* loadPlayer(const std::string& nick);
 
-    /*
+    /**
      * Update a player in the database
      *
-     * @param nick: The player's nickname
-     * @param game: The game stat to be updated
-     * @param toAddWin: If a win should be added
-     * @param toAddLoss: If a loss should be added
-     * @param (optional) name: The player's name to be changed
+     * @param nick The player's nickname
+     * @param game The game stat to be updated
+     * @param toAddWin If a win should be added
+     * @param toAddLoss If a loss should be added
+     * @param name (optional) The player's name to be changed
      */
     static int updatePlayer(const std::string& nick, char game, bool toAddWin, bool toAddLoss, const std::string& name = "");
 
-    /*
+    /**
      * Delete a player from the database
      *
-     * @param nick: The player's nickname
+     * @param nick The player's nickname
      *
-     * @return: DeletionStatus enum
+     * @return DeletionStatus enum
      */
     static DeletionStatus deletePlayer(const std::string& nick);
 
-    /*
+    /**
      * Load all players from the database
      *
-     * @return: A list with all players
+     * @return A list with all players
      */
     static std::list<Player> getAllPlayers();
 };
