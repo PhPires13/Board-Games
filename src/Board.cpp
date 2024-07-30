9a0d44cf209046c3a0e1ebd0c0b459447b3eb91f
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2024-07-30 13:48:27 -0300

    Add numbering in the board

--- src/Board.cpp
+++ src/Board.cpp
@@ -22,17 +22,27 @@ int Board::getHeight() const {
 }
 
 void Board::print() const {
-    for (int line = 0; line < this->height; line++) { // Each line
-        // Print upper border
-        if (line == 0) {
-            for (int column = 0; column < this->width; column++) { // Each column
-                std::cout << "=====";
-                if (column < this->width - 1) std::cout << '=';
-            }
-            std::cout << std::endl;
-        }
+    // Print column indexes above the lines
+    std::cout << "   ";
+    for (int column = 0; column < this->width; column++) {
+        std::cout << "  " << column << "  ";
+        if (column < this->width - 1) std::cout << ' ';
+    }
+    std::cout << std::endl;
 
-        /// Print the line content
+    // Print upper border
+    std::cout << "   ";
+    for (int column = 0; column < this->width; column++) { // Each column
+        std::cout << "=====";
+        if (column < this->width - 1) std::cout << '=';
+    }
+    std::cout << std::endl;
+
+    for (int line = 0; line < this->height; line++) { // Each line
+        // Print the line index
+        std::cout << line << "  ";
+
+        // Print the line content
         for (int column = 0; column < this->width; column++) { // Each column
             std::cout << "  " << this->board[line][column] << "  ";
             if (column < this->width - 1) std::cout << '|';
@@ -41,19 +51,22 @@ void Board::print() const {
 
         // Print the line border
         if (line < this->height - 1) { // Not the last line
+            std::cout << "   ";
             for (int column = 0; column < this->width; column++) { //
                 std::cout << "-----";
                 if (column < this->width - 1) std::cout << '+'; // Intersections
             }
             std::cout << std::endl;
-        } else { // Print the lower border
-            for (int column = 0; column < this->width; column++) { // Each column
-                std::cout << "=====";
-                if (column < this->width - 1) std::cout << '=';
-            }
-            std::cout << std::endl;
         }
     }
+
+    // Print the lower border
+    std::cout << "   ";
+    for (int column = 0; column < this->width; column++) { // Each column
+        std::cout << "=====";
+        if (column < this->width - 1) std::cout << '=';
+    }
+    std::cout << std::endl;
 }
 
 char Board::getSymbol(const int line, const int column) const {
