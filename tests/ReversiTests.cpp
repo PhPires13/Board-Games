38180a964eae19c86f0bb15c866825b7e62b84d2
Joaopaulo33 <joaopaulo33573@gmail.com>
2024-08-19 21:04:40 -0300

    Fix: Atualizando testes

--- tests/ReversiTests.cpp
+++ tests/ReversiTests.cpp
@@ -23,10 +23,10 @@ TEST_SUITE("Reversi") {
         CHECK_NOTHROW(Reversi(player1, player2));
 
         // Constructor with valid board size (even number)
-        CHECK_NOTHROW(Reversi(player1, player2, 6));
+        CHECK_NOTHROW(Reversi(player1, player2, 4));
 
         // Constructor with invalid board size (odd number), will sue the default size
-        CHECK_NOTHROW(Reversi(player1, player2, 6));
+        CHECK_NOTHROW(Reversi(player1, player2, 4));
 
         // Constructor with minimum valid board size
         CHECK_NOTHROW(Reversi(player1, player2, Reversi::minimumBoardSize));
@@ -49,10 +49,10 @@ TEST_SUITE("Reversi") {
         const Player player1("PED", "PEDRO");
         const Player player2("CARL", "CARLOS");
 
-        Reversi reversiGame(player1, player2, 6); // Tabuleiro 8x8
+        Reversi reversiGame(player1, player2, 4); // Tabuleiro 8x8
 
         // Movimentos válidos
-        CHECK_NOTHROW(reversiGame.validateMove({3, 4}));
+        CHECK_NOTHROW(reversiGame.validateMove({3, 2}));
 
         // Tamanho inválido
         CHECK_THROWS_AS(reversiGame.validateMove({}), incorrect_format);
@@ -73,12 +73,12 @@ TEST_SUITE("Reversi") {
         const Player player1("PED", "PEDRO", 'X');
         const Player player2("CARL", "CARLOS", 'O');
 
-        Reversi reversiGame(player1, player2, 6); // Tabuleiro 8x8
+        Reversi reversiGame(player1, player2, 4); // Tabuleiro 8x8
 
         // Executa o movimento e verifica se as peças são viradas corretamente
         reversiGame.makeMove({2, 3}, player1.getSymbol());
         CHECK(reversiGame.board.getSymbol(2, 3) == 'X');
-        CHECK(reversiGame.board.getSymbol(3, 3) == 'X');
+        CHECK(reversiGame.board.getSymbol(2, 2) == 'X');
     }
 
     TEST_CASE("Get Game State") {
@@ -91,12 +91,16 @@ TEST_SUITE("Reversi") {
         CHECK(reversiGame.getGameState({3, 2}) == GameState::NOT_OVER);
 
         // Exemplo de cenário onde o jogador 1 vence
-        reversiGame.makeMove({2, 3}, player1.getSymbol());
-        reversiGame.makeMove({2, 2}, player2.getSymbol());
-        reversiGame.makeMove({3, 2}, player1.getSymbol());
-        // reversiGame.makeMove({4, 2}, player2.getSymbol());
-        // reversiGame.makeMove({5, 2}, player1.getSymbol());
-        // CHECK(reversiGame.getGameState({3, 3}) == GameState::PLAYER1_WINS);
+        reversiGame.makeMove({3, 1}, player1.getSymbol());
+        reversiGame.makeMove({1, 0}, player2.getSymbol());
+        reversiGame.makeMove({0, 1}, player1.getSymbol());
+        reversiGame.makeMove({3, 2}, player2.getSymbol());
+        reversiGame.makeMove({3, 3}, player1.getSymbol());
+        reversiGame.makeMove({2, 3}, player2.getSymbol());
+        reversiGame.makeMove({1, 3}, player1.getSymbol());
+        reversiGame.makeMove({0, 3}, player2.getSymbol());
+
+        CHECK(reversiGame.getGameState({0, 2}) == GameState::PLAYER1_WINS);
 
     }
 
@@ -105,21 +109,19 @@ TEST_SUITE("Reversi") {
         const Player player1("PED", "PEDRO", 'X');
         const Player player2("CARL", "CARLOS", 'O');
 
-        // Exemplo de cenário onde o jogador 1 vence
         const std::istringstream input1("2 3\n2 2\n3 2\n4 2\n5 2\n"); // Entrada simulada
-        Reversi reversiGame(player1, player2, 5); // Tabuleiro 8x8
+        Reversi reversiGame(player1, player2, 4); // Tabuleiro 8x8
         GameState gameState;
         CHECK_NOTHROW(gameState = reversiGame.playGame());
         CHECK(gameState == GameState::PLAYER1_WINS);
 
-        // Restaura o std::cin para seu estado original
     }
 
     TEST_CASE("Game Tie") {
         const Player player1("PED", "PEDRO", 'X');
         const Player player2("CARL", "CARLOS", 'O');
 
-        Reversi reversiGame(player1, player2, 6); // Tabuleiro 8x8
+        Reversi reversiGame(player1, player2, 4); // Tabuleiro 8x8
 
         reversiGame.makeMove({3, 2}, player1.getSymbol());
         reversiGame.makeMove({2, 3}, player2.getSymbol());
